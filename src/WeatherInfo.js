818476c34003d06e5dca3d4cacdf583bd2b4abca
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-02-18 14:27:07 +0200

    updated weather info layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,29 +6,40 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="weather-data">
-      <div>
-        <h1 className="weather-app-city" id="city">
-          {props.data.city}
-        </h1>
-        <p className="weather-app-details">
-          <span id="time">
-            <FormattedDate date={props.data.date} />
-          </span>
-          ,<span id="weather-condition"> {props.data.description}</span>
-          <br />
-          Humidity: <strong id="humidity-data">{props.data.humidity}%</strong>,
-          Wind:
-          <strong id="speed-data">{props.data.wind}km/H</strong>
-        </p>
-      </div>
-      <div className="weather-app-temperature-container">
-        <div className="weather-app-icon" id="weather-app-icon">
-          <WeatherIcon code={props.data.icon} size={52} />
+      <div className="container">
+        <div className="row">
+          <div className="col-6 text-start">
+            <div>
+              <h1 className="weather-app-city" id="city">
+                {props.data.city}
+              </h1>
+              <p className="weather-app-details">
+                <span id="time">
+                  <FormattedDate date={props.data.date} />
+                </span>
+                <span id="weather-condition">{props.data.description}</span>
+                <br />
+                Humidity:{" "}
+                <strong id="humidity-data">{props.data.humidity}%</strong>,
+                Wind:
+                <strong id="speed-data">{props.data.wind}km/H</strong>
+              </p>
+            </div>
+          </div>
+          <div className="col-6 text-end">
+            <div className="d-flex weather-app-temperature-container">
+              <div className="weather-app-icon col-6" id="weather-app-icon">
+                <WeatherIcon code={props.data.icon} size={52} />
+              </div>
+              <div
+                className="weather-app-temperature text-end col-6"
+                id="weather-app-temperature"
+              >
+                <WeatherTemperature celsius={props.data.temperature} />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="weather-app-temperature" id="weather-app-temperature">
-          <WeatherTemperature celsius={props.data.temperature} />
-        </div>
-        <div className="weather-app-unit">°C | °F</div>
       </div>
     </div>
   );
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./WeatherInfo.css";
 export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
     </div>
   );
 }
