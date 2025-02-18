3a7358626f91cc5b0e8a582675a557b235f4b005
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-02-18 08:54:33 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather Forecast App - React</h1>
+      </header>
+      <h2>created by Sinesipho Sono</h2>
+    </div>
+  );
+}
+
+export default App;
